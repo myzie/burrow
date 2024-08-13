290152fe1c78146168a563d1000bb487ffdd07ff
Curtis Myzie <curtis.myzie@gmail.com>
2024-08-13 08:27:54 -0400

    NewClientWithOptions

--- client.go
+++ client.go
@@ -29,3 +29,34 @@ func NewRoundRobinClient(proxyURLs []string) *http.Client {
 		Transport: NewRoundRobinTransport(transports),
 	}
 }
+
+// ClientOpts is used to configure an http.Client with proxying and retry
+// behaviors.
+type ClientOpts struct {
+	ProxyURLs      []string
+	Retries        int
+	RetryableCodes []int
+}
+
+// NewClientWithOptions is a convenience function for creating an
+// http.Client with a Burrow transport that rotates through the provided
+// proxy URLs. It allows for configuration of retries via ClientOpts.
+func NewClientWithOptions(opts ClientOpts) *http.Client {
+	if len(opts.ProxyURLs) == 0 {
+		return &http.Client{}
+	}
+	var transports []http.RoundTripper
+	for _, proxyURL := range opts.ProxyURLs {
+		transports = append(transports, NewTransport(proxyURL, "POST"))
+	}
+	rr := NewRoundRobinTransport(transports)
+	if opts.Retries > 0 {
+		rr.WithRetries(opts.Retries)
+	}
+	if opts.RetryableCodes != nil {
+		rr.WithRetryableCodes(opts.RetryableCodes)
+	}
+	return &http.Client{
+		Transport: rr,
+	}
+}
